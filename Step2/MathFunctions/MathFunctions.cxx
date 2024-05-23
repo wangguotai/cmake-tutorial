b6aa63cf37645db051f927e03e00358e53be1b4a
王国泰 <wangguotai@xiaomi.com>
2024-05-23 12:01:21 +0800

    feat(step2): adding a Library

--- Step2/MathFunctions/MathFunctions.cxx
+++ Step2/MathFunctions/MathFunctions.cxx
@@ -4,17 +4,18 @@
 #include <cmath>
 // TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
 #ifdef USE_MYMATH
-# include "mysqrt.h"
+#include "mysqrt.h"
 #endif
-namespace mathfunctions {
-double sqrt(double x)
+namespace mathfunctions
 {
-  // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
-  // Otherwise, use std::sqrt.
-  #ifdef USE_MYMATH
+  double sqrt(double x)
+  {
+// TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
+// Otherwise, use std::sqrt.
+#ifdef USE_MYMATH
     return detail::mysqrt(x);
-  #else 
+#else
     return std::sqrt(x);
-  #endif
-}
+#endif
+  }
 }
